bfad2b040bd9351fa327d0190634eec012923520
Geordie-R <40744776+Geordie-R@users.noreply.github.com>
2020-03-09 00:54:39 +0000

    Rewrote takesnapshot.sh

--- takesnapshot.sh
+++ takesnapshot.sh
@@ -2,21 +2,20 @@
 ###### Parameter Section #####
 sshportno=466
 datename=$(date +%Y-%m-%d_%H-%M)
-shcreate=compressandsendlastsnapshot.sh
-cd ~
-compressedfolder=/root/data/snapshots/compressed/
+snapshotsfolder=/root/data/snapshots
+shcreate=$snapshotsfolder/compressandsendlastsnapshot.sh
+compressedfolder=$snapshotsfolder/compressed/
 filename="$compressedfolder$datename.tar.gz"
 mkdir -p $compressedfolder
+chmod +x $compressedfolder
 snapname=$(curl http://127.0.0.1:8888/v1/producer/create_snapshot | jq '.snapshot_name')
-
 
 rm -f $shcreate
 touch $shcreate && chmod +x $shcreate
 
 #List all .gz files especially the last 5 by filename aand remove the rest
-ls -F *.gz | head -n -5 | xargs -r rm
-
+ls -F $compressedfolder*.gz | head -n -5 | xargs -r rm
 echo "tar -cvzf $filename $snapname" >> $shcreate
 rm -f $snapname
 echo "rsync -rv -e 'ssh -p $sshportno' --progress $filename root@website.geordier.co.uk:/var/www/geordier.co.uk/snapshots" >> $shcreate
-./$shcreate
+$shcreate
